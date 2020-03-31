e026fb8c851fcf37d5e6f157db80ecfc12b47489
James Shvarts <jshvarts@gmail.com>
2020-03-31 09:58:40 -0400

    Add sort

--- app/src/main/java/com/example/coroutines/views/UserReposViewModel.kt
+++ app/src/main/java/com/example/coroutines/views/UserReposViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.coroutines.domain.Repo
 import com.example.coroutines.repository.UserReposRepository
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
@@ -21,13 +22,40 @@ class UserReposViewModel @Inject constructor(
     private val _isError = MutableLiveData<Boolean>(false)
     val isError: LiveData<Boolean> = _isError
 
+    // TODO handle this Channel
+    private val sortOrderChannel = ConflatedBroadcastChannel(SortOrder.ASCENDING)
+
     fun lookupUserRepos(login: String) {
 
         viewModelScope.launch {
             _userRepos.value = userReposRepository.getUserRepos(login)
                 .catch {
                     _isError.value = true
-                }.toList()
+                }
+                .toList()
+                .sortedByDescending { it.stars }
         }
     }
+
+    private suspend fun getUserRepos(login: String): List<Repo> {
+        return userReposRepository.getUserRepos(login)
+            .catch {
+                _isError.value = true
+            }
+            .toList()
+    }
+
+    fun changeSortOrder() {
+        val newSortOrder = if (sortOrderChannel.value == SortOrder.ASCENDING) {
+            SortOrder.DESCENDING
+        } else {
+            SortOrder.ASCENDING
+        }
+        sortOrderChannel.offer(newSortOrder)
+    }
 }
+
+private enum class SortOrder {
+    ASCENDING, DESCENDING;
+}
+
