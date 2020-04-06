1c2e74aa0312906b76465b2c0418316ebfab4914
James Shvarts <jshvarts@gmail.com>
2020-04-05 23:10:50 -0400

    Add repo filtering via Channel

--- app/src/test/java/com/example/coroutines/views/UserReposViewModelTest.kt
+++ app/src/test/java/com/example/coroutines/views/UserReposViewModelTest.kt
@@ -2,6 +2,8 @@ package com.example.coroutines.views
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.example.coroutines.domain.MinStarCount
+import com.example.coroutines.domain.NoMinStarCount
 import com.example.coroutines.domain.Repo
 import com.example.coroutines.domain.RepoOwner
 import com.example.coroutines.repository.UserReposRepository
@@ -34,6 +36,8 @@ class UserReposViewModelTest {
 
     private val isErrorObserver = mock<Observer<Boolean>>()
 
+    private val showSpinnerObserver = mock<Observer<Boolean>>()
+
     private lateinit var userReposViewModel: UserReposViewModel
 
     @Before
@@ -41,14 +45,15 @@ class UserReposViewModelTest {
         userReposViewModel = UserReposViewModel(userReposRepository).apply {
             userRepos.observeForever(userReposObserver)
             isError.observeForever(isErrorObserver)
+            showSpinner.observeForever(showSpinnerObserver)
         }
     }
 
     @Test
     fun `should emit user repos on success`() = rule.dispatcher.runBlockingTest {
         // GIVEN
-        val repo1 = Repo(name = "someRepo1", owner = RepoOwner(TEST_USERNAME), stars = 55)
-        val repo2 = Repo(name = "someRepo2", owner = RepoOwner(TEST_USERNAME), stars = 10)
+        val repo1 = Repo(name = "someRepo2", owner = RepoOwner(TEST_USERNAME), stars = 10)
+        val repo2 = Repo(name = "someRepo1", owner = RepoOwner(TEST_USERNAME), stars = 55)
         val expectedRepoList = listOf(repo1, repo2)
 
         val channel = Channel<List<Repo>>()
@@ -94,4 +99,58 @@ class UserReposViewModelTest {
         verify(userReposObserver).onChanged(expectedRepoList)
         verify(isErrorObserver).onChanged(true)
     }
+
+    @Test
+    fun `should emit user repos on success and filter by stars`() =
+        rule.dispatcher.runBlockingTest {
+            // GIVEN
+            val repo1 = Repo(name = "someRepo2", owner = RepoOwner(TEST_USERNAME), stars = 10)
+            val repo2 = Repo(name = "someRepo1", owner = RepoOwner(TEST_USERNAME), stars = 150)
+            val expectedRepoList = listOf(repo2)
+
+            val channel = Channel<List<Repo>>()
+            val flow = channel.consumeAsFlow()
+
+            doReturn(flow)
+                .whenever(userReposRepository)
+                .getUserRepos(TEST_USERNAME)
+
+            // WHEN
+            launch {
+                channel.send(listOf(repo1, repo2))
+            }
+
+            userReposViewModel.filterRepos(MinStarCount(100))
+            userReposViewModel.lookupUserRepos(TEST_USERNAME)
+
+            // THEN
+            verify(userReposObserver).onChanged(expectedRepoList)
+        }
+
+    @Test
+    fun `should emit user repos in correct order and without filter applied`() =
+        rule.dispatcher.runBlockingTest {
+            // GIVEN
+            val repo1 = Repo(name = "someRepo1", owner = RepoOwner(TEST_USERNAME), stars = 150)
+            val repo2 = Repo(name = "someRepo2", owner = RepoOwner(TEST_USERNAME), stars = 10)
+            val expectedRepoList = listOf(repo2, repo1)
+
+            val channel = Channel<List<Repo>>()
+            val flow = channel.consumeAsFlow()
+
+            doReturn(flow)
+                .whenever(userReposRepository)
+                .getUserRepos(TEST_USERNAME)
+
+            // WHEN
+            launch {
+                channel.send(listOf(repo1, repo2))
+            }
+
+            userReposViewModel.filterRepos(NoMinStarCount)
+            userReposViewModel.lookupUserRepos(TEST_USERNAME)
+
+            // THEN
+            verify(userReposObserver).onChanged(expectedRepoList)
+        }
 }
