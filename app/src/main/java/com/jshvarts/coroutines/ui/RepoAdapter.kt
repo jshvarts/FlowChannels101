14a5530f48d259086d75b8d8be2bb7ed1e36bb00
James Shvarts <jshvarts@gmail.com>
2020-09-23 08:08:37 -0400

    Move click listener to view holder init

--- app/src/main/java/com/jshvarts/coroutines/ui/RepoAdapter.kt
+++ app/src/main/java/com/jshvarts/coroutines/ui/RepoAdapter.kt
@@ -1,6 +1,7 @@
 package com.jshvarts.coroutines.ui
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
@@ -12,55 +13,64 @@ import com.jshvarts.coroutines.domain.Repo
 import com.jshvarts.coroutines.domain.RepoOwner
 import com.squareup.picasso.Picasso
 
+private typealias RepoClickListener = (RepoOwner, ImageView) -> Unit
 private const val AVATAR_WIDTH = 250
 
-class RepoAdapter(private val clickListener: (RepoOwner, ImageView) -> Unit) :
-    ListAdapter<Repo, RepoAdapter.RepoViewHolder>(RepoDiffCallback()) {
+class RepoAdapter(
+    private val clickListener: RepoClickListener
+) : ListAdapter<Repo, RepoViewHolder>(RepoDiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): RepoViewHolder {
         val binding = RepoItemBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
         )
-        return RepoViewHolder(binding)
+        return RepoViewHolder(binding, clickListener)
     }
 
     override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
+}
 
-    inner class RepoViewHolder(
-        binding: RepoItemBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
+class RepoViewHolder(
+    binding: RepoItemBinding,
+    private val clickListener: RepoClickListener
+) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
 
-        private val repoNameView = binding.repoName
-        private val starsView = binding.stars
-        private val repoOwnerView = binding.repoOwner
-        private val avatarImageView = binding.avatarImageView
+    private lateinit var item: Repo
 
-        fun bind(item: Repo) {
-            repoNameView.text = item.name
-            starsView.text = itemView.resources.getQuantityString(
-                R.plurals.repo_stars, 0, item.stars
-            )
+    private val repoNameView = binding.repoName
+    private val starsView = binding.stars
+    private val repoOwnerView = binding.repoOwner
+    private val avatarImageView = binding.avatarImageView
 
-            avatarImageView.apply {
-                Picasso.get()
-                    .load(item.owner.avatarUrl)
-                    .resize(AVATAR_WIDTH, AVATAR_WIDTH)
-                    .centerCrop()
-                    .into(this)
-                transitionName = item.owner.login
-                setOnClickListener {
-                    clickListener.invoke(item.owner, this)
-                }
-            }
+    init {
+        avatarImageView.setOnClickListener(this)
+        repoOwnerView.setOnClickListener(this)
+    }
 
-            repoOwnerView.text = item.owner.login
-            repoOwnerView.setOnClickListener {
-                clickListener.invoke(item.owner, avatarImageView)
-            }
+    fun bind(item: Repo) {
+        this.item = item
+        repoNameView.text = item.name
+        starsView.text = itemView.resources.getQuantityString(
+            R.plurals.repo_stars, 0, item.stars
+        )
+
+        avatarImageView.apply {
+            Picasso.get()
+                .load(item.owner.avatarUrl)
+                .resize(AVATAR_WIDTH, AVATAR_WIDTH)
+                .centerCrop()
+                .into(this)
+            transitionName = item.owner.login
         }
+
+        repoOwnerView.text = item.owner.login
+    }
+
+    override fun onClick(v: View?) {
+        v?.let { clickListener.invoke(item.owner, avatarImageView) }
     }
 }
 
